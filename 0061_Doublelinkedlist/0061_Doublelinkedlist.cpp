fbe97c5778251dce1173efce6d065cc69ca0a0b5
NaufalZuhdyRabbani <naufalkzrz@gmail.com>
2024-05-05 05:48:55 +0700

    delete current

--- 0061_Doublelinkedlist/0061_Doublelinkedlist.cpp
+++ 0061_Doublelinkedlist/0061_Doublelinkedlist.cpp
@@ -112,3 +112,12 @@ void deleteNode()
 			current->next->prev = previous;
 		}
 	}
+
+	delete current;
+	cout << "\x1b[32mRecord with roll number " << rollNo << " delete\x1b[0m" << endl;
+}
+
+bool listEmpty()
+{
+	return (START == NULL);
+}
